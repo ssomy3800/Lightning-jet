3364813a8ade7b56bdace50e9f7fdfca45252fbd
ssomy3800 <ssomy3800@gmail.com>
2023-06-21 14:04:13 -0700

    fixed favicon, gave a mute button icon

--- src/index.js
+++ src/index.js
@@ -18,11 +18,20 @@ function startGame() {
     if (isMuted) {
       bgm.muted = false;
       bossBgm.muted = false;
-      muteButton.innerText = "Mute";
+      muteButton.innerHTML = `
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="30px"
+          width="30px"
+          viewBox="0 0 640 512"
+        >
+          <path d="M533.6 32.5C598.5 85.3 640 165.8 640 256s-41.5 170.8-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 331.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z" />
+        </svg>
+      `;
     } else {
       bgm.muted = true;
       bossBgm.muted = true;
-      muteButton.innerText = "Unmute";
+      muteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="30px" width="30px" viewBox="0 0 576 512"><path d="M301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3zM425 167l55 55 55-55c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-55 55 55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-55-55-55 55c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l55-55-55-55c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0z"/></svg>`;
     }
   });
   const app = new PIXI.Application({
@@ -106,12 +115,22 @@ function startGame() {
       if (isPaused) {
         app.ticker.start();
         if (isAudioPlaying(bossBgm)) {
-          bossBgm.resume();
+          bossBgm
+            .play()
+            .catch((error) => console.error("Playback failed due to: ", error));
         } else if (!isAudioPlaying(bgm)) {
           if (bossJet) {
-            bossBgm.play();
+            bossBgm
+              .play()
+              .catch((error) =>
+                console.error("Playback failed due to: ", error)
+              );
           } else {
-            bgm.play();
+            bgm
+              .play()
+              .catch((error) =>
+                console.error("Playback failed due to: ", error)
+              );
           }
         }
       } else {
@@ -136,13 +155,17 @@ function startGame() {
       app.ticker.stop();
       displayDefeatPicture();
       bossBgm.pause();
-      bgm.play();
+      bgm
+        .play()
+        .catch((error) => console.error("Playback failed due to: ", error));
     } else if (bossJet) {
       if (!bossJet.bossAlive) {
         app.ticker.stop();
         displayVictoryPicture();
         bossBgm.pause();
-        bgm.play();
+        bgm
+          .play()
+          .catch((error) => console.error("Playback failed due to: ", error));
       }
     }
   }
@@ -243,23 +266,6 @@ function startGame() {
 
   const enemyJets = [];
 
-  // Add this function to spawn a group of 5 enemy jets
-  // function spawnEnemyJets() {
-  //   for (let i = 0; i < 1; i++) {
-  //     const enemyJet = new EnemyJet(
-  //       app.view.width,
-  //       (Math.random() * app.view.height) / 3,
-  //       enemyJetTexture,
-  //       enemyBullets,
-  //       app,
-  //       "common",
-  //       -1,
-  //       0
-  //     );
-  //     enemyJets.push(enemyJet);
-  //     app.stage.addChild(enemyJet.sprite);
-  //   }
-  // }
   function spawnMiniBoss() {
     const miniBossJet = new EnemyJet(
       app.view.width,
@@ -314,7 +320,9 @@ function startGame() {
       if (currentWave > totalWaves) {
         spawnBossJets();
         bgm.pause();
-        bossBgm.play();
+        bossBgm
+          .play()
+          .catch((error) => console.error("Playback failed due to: ", error));
         clearInterval(spawnInterval);
         return;
       }
@@ -342,16 +350,19 @@ function startGame() {
     // Depending on the wave, spawn enemies from the right or left.
     const spawnDirection = wave % 2 === 0 ? "right" : "left";
 
-    // Use setInterval to spawn common jets every 2 seconds.
-    const commonJetSpawnInterval = setInterval(() => {
-      spawnEnemyJets(spawnDirection);
-      currentEnemy++;
+    // Only start a new interval if the enemyJets array is empty.
+    if (!enemyJets.length) {
+      // Use setInterval to spawn common jets every 2 seconds.
+      const commonJetSpawnInterval = setInterval(() => {
+        spawnEnemyJets(spawnDirection);
+        currentEnemy++;
 
-      // If we've spawned all enemies for the current wave, clear the interval.
-      if (currentEnemy >= enemiesPerWave) {
-        clearInterval(commonJetSpawnInterval);
-      }
-    }, commonJetInterval);
+        // If we've spawned all enemies for the current wave, clear the interval.
+        if (currentEnemy >= enemiesPerWave) {
+          clearInterval(commonJetSpawnInterval);
+        }
+      }, commonJetInterval);
+    }
   }
 
   function spawnEnemyJets(direction) {
